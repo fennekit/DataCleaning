0432f13ce88872e62dd7a28b9bd6a50cf9c38cda
fennekit <erik.konijnenburg@fennek-it.nl>
2016-08-15 11:46:48 +0200

    updated reading

--- run_analysis.R
+++ run_analysis.R
@@ -8,13 +8,17 @@ if (!file.exists(datafile)) {
     url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
     download.file(url, datafile, method="curl")
 }
-extractDir <- "output"
+extractDir <- "zipdata"
 #unzip the file overwriting the files to ensure that any modifications are lost
 unzip(datafile, overwrite = TRUE, exdir = extractDir)
 
 readDir <- paste(extractDir, "/UCI HAR Dataset/", sep="")
 
-trainSet <- read.table(paste(readDir,"train/X_train.txt", sep=""), header=FALSE, sep=" ")
-trainLabels <- read.table(paste(readDir,"train/y_train.txt", sep=""), header=FALSE, sep = " ")
-testSet <- read.table(paste(readDir,"test/X_test.txt", sep=""), header=FALSE, sep = " ")
-testLabels <- read.table(paste(readDir,"test/y_test.txt", sep=""), header=FALSE, sep = " ")
+#read datacolumns and determine count
+dataColumns <- read.table(paste(readDir, "features.txt"))
+
+trainSet <- read.table(paste(readDir,"train/X_train.txt", sep=""), header=FALSE)
+testSet <- read.table(paste(readDir,"test/X_test.txt", sep=""), header=FALSE)
+
+trainLabels <- read.table(paste(readDir,"train/y_train.txt", sep=""), header=FALSE)
+testLabels <- read.table(paste(readDir,"test/y_test.txt", sep=""), header=FALSE)
