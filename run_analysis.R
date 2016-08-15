10aba6fa3b41d3d008cc02eff716754e6476f61a
fennekit <erik.konijnenburg@fennek-it.nl>
2016-08-15 13:56:50 +0200

    Added reading filtering merging of sets

--- run_analysis.R
+++ run_analysis.R
@@ -15,10 +15,26 @@ unzip(datafile, overwrite = TRUE, exdir = extractDir)
 readDir <- paste(extractDir, "/UCI HAR Dataset/", sep="")
 
 #read datacolumns and determine count
-dataColumns <- read.table(paste(readDir, "features.txt"))
+dataColumns <- read.table(paste(readDir, "features.txt", sep=""), header = FALSE)
+meanAndStdColumns <- grep("mean|std",dataColumns[,2])
+meanAndStdColumns.Names <- as.character(dataColumns[,2][meanAndStdColumns])
+meanAndStdColumns.Names <- sub("-mean", "Mean", meanAndStdColumns.Names)
+meanAndStdColumns.Names <- sub("-std", "Std", meanAndStdColumns.Names)
+meanAndStdColumns.Names <- sub("\\(\\)", "", meanAndStdColumns.Names)
+meanAndStdColumns.Names <- sub("^t", "Time",meanAndStdColumns.Names)
+meanAndStdColumns.Names <- sub("^f", "Frequency",meanAndStdColumns.Names)
 
-trainSet <- read.table(paste(readDir,"train/X_train.txt", sep=""), header=FALSE)
-testSet <- read.table(paste(readDir,"test/X_test.txt", sep=""), header=FALSE)
+trainSet <- read.table(paste(readDir,"train/X_train.txt", sep=""), header=FALSE)[meanAndStdColumns]
+testSet <- read.table(paste(readDir,"test/X_test.txt", sep=""), header=FALSE)[meanAndStdColumns]
 
 trainLabels <- read.table(paste(readDir,"train/y_train.txt", sep=""), header=FALSE)
 testLabels <- read.table(paste(readDir,"test/y_test.txt", sep=""), header=FALSE)
+
+trainSubject <-read.table(paste(readDir,"train/subject_train.txt", sep=""), header=FALSE)
+testSubject <-read.table(paste(readDir,"test/subject_test.txt", sep=""), header=FALSE)
+
+train <- cbind(trainSubject, trainLabels, trainSet)
+test <- cbind(testSubject, testLabels, testSet)
+
+total <- rbind(train, test)
+names(total) <- c("Subject", "Activity", meanAndStdColumns.Names)
