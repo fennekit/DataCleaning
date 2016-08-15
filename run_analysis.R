d73c2853a73c5bf193cf0fceb5cac30f42399b5a
fennekit <erik.konijnenburg@fennek-it.nl>
2016-08-15 14:38:03 +0200

    Create total set

--- run_analysis.R
+++ run_analysis.R
@@ -14,9 +14,11 @@ unzip(datafile, overwrite = TRUE, exdir = extractDir)
 
 readDir <- paste(extractDir, "/UCI HAR Dataset/", sep="")
 
-#read datacolumns and determine count
+#read datacolumns and determine the ones we are interested in (std and mean)
 dataColumns <- read.table(paste(readDir, "features.txt", sep=""), header = FALSE)
 meanAndStdColumns <- grep("mean|std",dataColumns[,2])
+
+#create nice names
 meanAndStdColumns.Names <- as.character(dataColumns[,2][meanAndStdColumns])
 meanAndStdColumns.Names <- sub("-mean", "Mean", meanAndStdColumns.Names)
 meanAndStdColumns.Names <- sub("-std", "Std", meanAndStdColumns.Names)
@@ -24,17 +26,32 @@ meanAndStdColumns.Names <- sub("\\(\\)", "", meanAndStdColumns.Names)
 meanAndStdColumns.Names <- sub("^t", "Time",meanAndStdColumns.Names)
 meanAndStdColumns.Names <- sub("^f", "Frequency",meanAndStdColumns.Names)
 
+#read the train and test set
 trainSet <- read.table(paste(readDir,"train/X_train.txt", sep=""), header=FALSE)[meanAndStdColumns]
 testSet <- read.table(paste(readDir,"test/X_test.txt", sep=""), header=FALSE)[meanAndStdColumns]
 
+#read the labels for the training set and match them with names
 trainLabels <- read.table(paste(readDir,"train/y_train.txt", sep=""), header=FALSE)
-testLabels <- read.table(paste(readDir,"test/y_test.txt", sep=""), header=FALSE)
+trainLabels <- merge(trainLabels, labels, by.x = "V1", by.y="V1")
+names(trainLabels) <- c("Id", "Activity")
 
+#read the labels for the test and match them with names
+testLabels <- read.table(paste(readDir,"test/y_test.txt", sep=""), header=FALSE)
+testLabels <- merge(testLabels, labels, by.x = "V1", by.y="V1")
+names(testLabels) <- c("Id", "Activity")
+
+#read the subject data
 trainSubject <-read.table(paste(readDir,"train/subject_train.txt", sep=""), header=FALSE)
 testSubject <-read.table(paste(readDir,"test/subject_test.txt", sep=""), header=FALSE)
 
-train <- cbind(trainSubject, trainLabels, trainSet)
-test <- cbind(testSubject, testLabels, testSet)
+#create a total set for the training data
+train <- cbind(trainSubject, trainLabels$Activity, trainSet)
+names(train) <- c("Subject", "Activity", meanAndStdColumns.Names)
 
+#create a total set for the test data
+test <- cbind(testSubject, testLabels$Activity, testSet)
+names(test) <- c("Subject", "Activity", meanAndStdColumns.Names)
+
+#create a total set
 total <- rbind(train, test)
-names(total) <- c("Subject", "Activity", meanAndStdColumns.Names)
+
